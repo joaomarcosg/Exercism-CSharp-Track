b72560de072d9e753c8857fa6e862e1ce950919d
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-19 17:08:40 -0300

    FEAT: add solution to Hello World exercise

---
+++ hello/HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpTrack.hello
+{
+    public class HelloWorld
+    {
+        public static string Hello()
+        {
+            return "Hello, World!";
+        }
+    }
+}
